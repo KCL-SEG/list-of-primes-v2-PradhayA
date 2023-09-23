f5de9578079e03c0c1bd89638cfacb2586239e75
PradhayA <meetpradhay@gmail.com>
2023-09-23 21:58:10 +0100

    add test for negative

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if (number_of_primes <= 0):
+        raise ValueError()
+    list = [2]
+    firstNum = 3
+    while len(list)!= number_of_primes:
+        if checkIfPrime(firstNum):
+            list.append(firstNum)
+        firstNum = firstNum + 1
     return list
+
+def checkIfPrime(num):
+    for i in range(2, num//2+1):
+        if (num%i == 0):
+            return False
+    return True
